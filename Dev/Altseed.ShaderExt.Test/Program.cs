4fb9534c0ab21f21b117b1f5cdf6dbefa72430ba
wraikny <writtenknown@gmail.com>
2019-08-19 14:48:32 +0900

    wip

--- Dev/Altseed.ShaderExt.Test/Program.cs
+++ Dev/Altseed.ShaderExt.Test/Program.cs
@@ -12,38 +12,6 @@ namespace Altseed.ShaderExt.Test
         {
             asd.Engine.Initialize("Altseed.ShaderExt.Test", 800, 450, new asd.EngineOption());
 
-            var obj = new asd.TextureObject2D();
-
-            string shaderString = @"
-Texture2D g_texture : register( t0 );
-SamplerState g_sampler : register( s0 );
-
-struct PS_Input
-{
-    float4 SV_Position : SV_POSITION;
-    float4 Position : POSITION;
-    float2 UV : UV;
-    float4 Color : COLOR;
-};
-
-float4 main( const PS_Input Input ) : SV_Target
-{
-    float4 color = g_texture.Sample(g_sampler, Input.UV);
-    return float4( 1.0 - color.x, 1.0 - color.y, 1.0 - color.z, color.w);
-}
-";
-
-            var shader = asd.Engine.Graphics.CreateShader2D(shaderString);
-            var mat = asd.Engine.Graphics.CreateMaterial2D(shader);
-            var texture = Altseed.ShaderExt.Utils.CreateTexture2D("wraikny_icon.jpg");
-            mat.SetTexture2D("g_texture", texture);
-            mat.SetTextureFilterType("g_texture", asd.TextureFilterType.Linear);
-            mat.SetTextureWrapType("g_texture", asd.TextureWrapType.Repeat);
-            //obj.Material = mat;
-            obj.Texture = texture;
-
-            asd.Engine.AddObject2D(obj);
-
             while(asd.Engine.DoEvents())
             {
                 asd.Engine.Update();
--- Dev/Altseed.ShaderExt/Utils.cs
+++ Dev/Altseed.ShaderExt/Utils.cs
@@ -11,27 +11,35 @@ namespace Altseed.ShaderExt
     {
         public static string LoadFile(string path)
         {
-            if(!asd.Engine.File.Exists("Altseed.ShaderExt.Shaders/license.txt"))
+            var filename = "Altseed.ShaderExt.Shaders/" + path;
+
+            if (!asd.Engine.File.Exists(filename))
             {
                 asd.Engine.File.AddRootPackage("Altseed.ShaderExt.Shaders.pack");
+
+                if (!asd.Engine.File.Exists(filename))
+                {
+                    throw new FileNotFoundException();
+                }
             }
 
-            var buf = asd.Engine.File.CreateStaticFile("Altseed.ShaderExt.Shaders/" + path).Buffer;
+            var buf = asd.Engine.File.CreateStaticFile(filename).Buffer;
             return Encoding.UTF8.GetString(buf);
         }
 
         public static asd.Texture2D CreateTexture2D(string path)
         {
-            if (!asd.Engine.File.Exists("Altseed.ShaderExt.Shaders/license.txt"))
-            {
-                asd.Engine.File.AddRootPackage("Altseed.ShaderExt.Shaders.pack");
-            }
 
             var filename = "Altseed.ShaderExt.Shaders/" + path;
 
             if(!asd.Engine.File.Exists(filename))
             {
-                throw new FileNotFoundException();
+                asd.Engine.File.AddRootPackage("Altseed.ShaderExt.Shaders.pack");
+
+                if(!asd.Engine.File.Exists(filename))
+                {
+                    throw new FileNotFoundException();
+                }
             }
 
             return asd.Engine.Graphics.CreateTexture2D(filename);

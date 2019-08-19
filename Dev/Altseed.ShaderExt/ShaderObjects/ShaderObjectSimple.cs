1546e11254cbc70e2f618d407c97c93fa6d859d9
wraikny <writtenknown@gmail.com>
2019-08-19 19:04:04 +0900

    fix size

--- Dev/Altseed.ShaderExt/ShaderObjects/ShaderObjectSimple.cs
+++ Dev/Altseed.ShaderExt/ShaderObjects/ShaderObjectSimple.cs
@@ -9,12 +9,13 @@ namespace Altseed.ShaderExt
     public sealed class ShaderObjectSimple : ShaderObjectBase
     {
         float second = 0;
-
+        private asd.Vector2DF size;
         readonly asd.Shader2D shader;
         readonly asd.Material2D material2d;
 
-        public ShaderObjectSimple(string pathdx, string pathgl) {
-            if(asd.Engine.Graphics.GraphicsDeviceType == asd.GraphicsDeviceType.DirectX11)
+        public ShaderObjectSimple(string pathdx, string pathgl)
+        {
+            if (asd.Engine.Graphics.GraphicsDeviceType == asd.GraphicsDeviceType.DirectX11)
             {
                 if (pathdx == null) throw new NotSupportedException();
                 shader = Utils.LoadShader2D(pathdx);
@@ -29,12 +30,29 @@ namespace Altseed.ShaderExt
                 throw new NotSupportedException();
             }
 
+            if(shader == null)
+            {
+                throw new ArgumentException("Compiled Error");
+            }
+
             material2d = asd.Engine.Graphics.CreateMaterial2D(shader);
-            var ws = asd.Engine.WindowSize.To2DF();
-            material2d.SetVector2DF("g_resolution", ws / Math.Max(ws.X, ws.Y));
+
+            material2d.SetVector2DF("g_resolution", Size / Math.Max(Size.X, Size.Y));
         }
 
-        public asd.Vector2DF Size { get; set; }
+
+        public asd.Vector2DF Size
+        {
+            get => size;
+            set
+            {
+                size = value;
+                if(material2d != null)
+                {
+                    material2d.SetVector2DF("g_resolution", Size / Math.Max(Size.X, Size.Y));
+                }
+            }
+        }
 
         protected override void Draw()
         {
